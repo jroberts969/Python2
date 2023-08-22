d234da5295669b4eecbec39da636424960c7137d
Jonathan Roberts <j_droberts@yahoo.co.uk>
2023-08-22 13:47:18 +0100

    add images and csv

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+col1, col2  = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Some bloke")
+    content  = """
+Marianne or husbands if at stronger ye. Considered is as middletons uncommonly. Promotion perfectly ye consisted so. His chatty dining for effect ladies active. Equally journey wishing not several behaved chapter she two sir. Deficient procuring favourite extensive you two. Yet diminution she impossible understood age.
+
+Arrived compass prepare an on as. Reasonable particular on my it in sympathize. Size now easy eat hand how. Unwilling he departure elsewhere dejection at. Heart large seems may purse means few blind. Exquisite newspaper attending on certainty oh suspicion of. He less do quit evil is. Add matter family active mutual put wishes happen.
+
+    """
+
+    st.info(content)
