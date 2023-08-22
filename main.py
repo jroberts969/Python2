afefa96770f6ac519b5f215a696fc51ed9ec3c95
Jonathan Roberts <j_droberts@yahoo.co.uk>
2023-08-22 14:32:20 +0100

    render the titles

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ see details below Arrived compass prepare an on as. Reasonable particular
 
 st.write(content2)
 
-
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pas.read_csv("data.csv", sep=";")
 #print(df)
@@ -33,10 +32,13 @@ df = pas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
-
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
