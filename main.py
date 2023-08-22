8c6fbc352bc27cebd124d3af163df175594cfa02
Jonathan Roberts <j_droberts@yahoo.co.uk>
2023-08-22 14:15:20 +0100

    render the titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -22,3 +23,20 @@ see details below Arrived compass prepare an on as. Reasonable particular
 """
 
 st.write(content2)
+
+
+col3, col4 = st.columns(2)
+
+df = pas.read_csv("data.csv", sep=";")
+#print(df)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
