9b884c6c4569c678216e9f0fcc5b122b8fc766c3
Jonathan Roberts <j_droberts@yahoo.co.uk>
2023-08-22 19:41:17 +0100

    working version

--- pages/Contacts_Us.py
+++ pages/Contacts_Us.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact me")
 
 with st.form(key="email_form"):
-    user_email = st.text_input("Your Email Address")
+    user_email = st.text_input("Your Email Address", value="j_droberts@yahoo.co.uk" )
     user_message = st.text_area("Your message")
     button = st.form_submit_button()
 
     if button:
         print("I was pressed")
+        send_email(receiver=user_email, message=user_message)
--- send_email.py
+++ send_email.py
@@ -1,21 +1,21 @@
 import smtplib, ssl
 
-password = "eeldhzhbszwnpeid"
-username = "dr.jonathan.roberts@googlemail.com"
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(receiver, message):
+    password = "eeldhzhbszwnpeid"
+    username = "dr.jonathan.roberts@googlemail.com"
+    host = "smtp.gmail.com"
+    port = 465
+    #receiver = "j_droberts@yahoo.co.uk"
+    context = ssl.create_default_context()
+    #message1 = "hi, how are you"
+    message_to_send = f'''Subject: A plain text email
+   ''' + message
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message_to_send)
+        print("success!")
 
 
-receiver = "j_droberts@yahoo.co.uk"
-context = ssl.create_default_context()
-message  = "hi, how are you"
 
-email_string = f'''Subject: A plain text email
-This is a new test email sent by Python.'''
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, email_string )
-
-print("success")
