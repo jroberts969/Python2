62ba0dfc10337675c6eae3259fdc434a6b1bf6ea
Jonathan Roberts <j_droberts@yahoo.co.uk>
2023-08-25 11:48:15 +0100

    final commit

--- send_email.py
+++ send_email.py
@@ -1,19 +1,17 @@
 import smtplib, ssl
-
+import os
 
 def send_email(receiver, message, option):
-    password = "eeldhzhbszwnpeid"
+    #password = "eeldhzhbszwnpeid"
+    password = os.getenv("PASSWORD")
+
     username = "dr.jonathan.roberts@googlemail.com"
     host = "smtp.gmail.com"
     port = 465
-    #receiver = "j_droberts@yahoo.co.uk"
     context = ssl.create_default_context()
-    #message1 = "hi, how are you"
     message_to_send = f'''Subject: {option}
 {message} , {username}'''
 
-    print(option)
-    print(message_to_send)
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
